4ea72218f325d1a5315e827e6f3d2b8dbdc708d2
Andrei Tkach <st1793688@stud.duikt.edu.ua>
2025-06-16 21:21:54 +0200

    Initial commit with both projects (student email)

--- Models/JwtSettings.cs
+++
@@ -1,8 +0,0 @@
-namespace PasswordManagerFull.Models
-{
-    public class JwtSettings
-    {
-        public string SecretKey { get; set; } = string.Empty;
-        public int ExpiryInMinutes { get; set; }
-    }
-} 
--- Models/MongoDbSettings.cs
+++
@@ -1,8 +0,0 @@
-namespace PasswordManagerFull.Models
-{
-    public class MongoDbSettings
-    {
-        public string ConnectionString { get; set; } = string.Empty;
-        public string DatabaseName { get; set; } = string.Empty;
-    }
-} 
